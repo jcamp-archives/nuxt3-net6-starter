2ee13a52b7321ff76df2e6fb0346cd23010f60a8
John Campion <john@brightshore.com>
2022-08-04 16:42:26 -0400

    restore 404 page

---
+++ src/nuxt-app/pages/404.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const router = useRouter()
+</script>
+
+<template>
+  <main class="px-4 py-10 text-center teal-700 dark:gray-200">
+    <div class="text-4xl">
+      <div class="i-carbon-warning inline-block" />
+    </div>
+    <div>Not found</div>
+    <div>
+      <button class="btn text-sm m-3 t-8" @click="router.back()">
+        Back
+      </button>
+    </div>
+  </main>
+</template>
