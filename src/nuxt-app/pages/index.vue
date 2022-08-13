b74b657e41cea310082223a4818def9519c1b6e6
John Campion <john@brightshore.com>
2022-08-12 20:04:39 -0400

    Added some extra icons
    for testing new tailwind icon plugin

--- src/nuxt-app/pages/index.vue
+++ src/nuxt-app/pages/index.vue
@@ -2,5 +2,25 @@
   <div>
     <Logos class="mb-6" />
     <InputEntry />
+    <div class="text-gray4 justify-left m-5 flex gap-3 text-xl">
+      <NuxtLink
+        class="i-carbon-campsite"
+        to="/"
+      />
+      <NuxtLink
+        class="i-carbon-temperature"
+        to="/fetch-data"
+      />
+      <NuxtLink
+        class="i-carbon-data-error"
+        to="/validation"
+      />
+      <a
+        class="i-carbon-logo-github"
+        href="https://github.com/JohnCampionJr/nuxt3-net6-starter"
+        target="_blank"
+      />
+      <DarkToggle />
+    </div>
   </div>
 </template>
